7da9d2aba73498a018312d19e976faf7f76ae9c6
homuler <eulerdora.1810.mann@gmail.com>
2020-11-29 18:32:03 +0900

    feat!(example): get output packets asynchronously

--- Assets/MediaPipe/Examples/Scripts/ObjectDetection/ObjectDetectionGraph.cs
+++ Assets/MediaPipe/Examples/Scripts/ObjectDetection/ObjectDetectionGraph.cs
@@ -1,34 +1,59 @@
 using Mediapipe;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using System.Runtime.InteropServices;
 
 public class ObjectDetectionGraph : DemoGraph {
   private const string outputDetectionsStream = "output_detections";
-  private OutputStreamPoller<List<Detection>> outputDetectionsStreamPoller;
-  private DetectionVectorPacket outputDetectionsPacket;
+
+  private Stack<List<Detection>> detectionLists;
+  private GCHandle outputDetectionsCallbackHandle;
 
   public override Status StartRun() {
-    outputDetectionsStreamPoller = graph.AddOutputStreamPoller<List<Detection>>(outputDetectionsStream).ConsumeValueOrDie();
-    outputDetectionsPacket = new DetectionVectorPacket();
+    detectionLists = new Stack<List<Detection>>();
+    graph.ObserveOutputStream<DetectionVectorPacket, List<Detection>>(
+        outputDetectionsStream, OutputDetectionsCallback, out outputDetectionsCallbackHandle).AssertOk();
 
     return graph.StartRun();
   }
 
+  protected override void OnDestroy() {
+    base.OnDestroy();
+
+    if (outputDetectionsCallbackHandle.IsAllocated) {
+      outputDetectionsCallbackHandle.Free();
+    }
+  }
+
   public override void RenderOutput(WebCamScreenController screenController, PixelData pixelData) {
-    var detections = FetchNextOutputDetections();
-    RenderAnnotation(screenController, detections);
+    List<Detection> detections = null;
+
+    lock (((ICollection)detectionLists).SyncRoot) {
+      if (detectionLists.Count > 0) {
+        detections = detectionLists.Peek();
+        detectionLists.Clear();
+      }
+    }
+
+    RenderAnnotation(screenController, detections == null ? new List<Detection>() : detections);
 
     var texture = screenController.GetScreen();
     texture.SetPixels32(pixelData.Colors);
     texture.Apply();
   }
 
-  private List<Detection> FetchNextOutputDetections() {
-    return FetchNextVector<Detection>(outputDetectionsStreamPoller, outputDetectionsPacket, outputDetectionsStream);
-  }
-
   private void RenderAnnotation(WebCamScreenController screenController, List<Detection> detections) {
     // NOTE: input image is flipped
     GetComponent<DetectionListAnnotationController>().Draw(screenController.transform, detections, true);
+  }
+
+  private Status OutputDetectionsCallback(DetectionVectorPacket packet) {
+    var value = packet.Get();
+
+    lock (((ICollection)detectionLists).SyncRoot) {
+      detectionLists.Push(value);
+    }
+
+    return Status.Ok();
   }
 }
